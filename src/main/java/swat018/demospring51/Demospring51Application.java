487ee6d8bab56667181322c14faa22ca6a880bb9
jinwoo park <swat018@gmail.com>
2021-06-24 23:27:15 +0900

    IoC 컨테이너 6부: Environment.프로퍼티

--- src/main/java/swat018/demospring51/Demospring51Application.java
+++ src/main/java/swat018/demospring51/Demospring51Application.java
@@ -2,8 +2,10 @@ package swat018.demospring51;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.PropertySource;
 
 @SpringBootApplication
+@PropertySource("classpath:/app.properties")
 public class Demospring51Application {
     public static void main(String[] args) {
         SpringApplication.run(Demospring51Application.class, args);
