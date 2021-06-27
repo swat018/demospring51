6a7012da165b62bb01d521d6eb3a252a5fdec44c
jinwoo park <swat018@gmail.com>
2021-06-27 09:28:39 +0900

    IoC 컨테이너 8부: ApplicationEventPublisher

--- src/main/java/swat018/demospring51/AppRunner.java
+++ src/main/java/swat018/demospring51/AppRunner.java
@@ -3,23 +3,17 @@ package swat018.demospring51;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.context.MessageSource;
+import org.springframework.context.ApplicationEventPublisher;
 import org.springframework.stereotype.Component;
-
-import java.util.Locale;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    MessageSource messageSource;
+    ApplicationEventPublisher publishEvent;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        while(true) {
-            System.out.println(messageSource.getMessage("greeting", new String[]{"jinwoo"}, Locale.KOREA));
-            System.out.println(messageSource.getMessage("greeting", new String[]{"jinwoo"}, Locale.getDefault()));
-            Thread.sleep(1000L);
-        }
+            publishEvent.publishEvent(new MyEvent(this, 100));
     }
 }
--- src/main/java/swat018/demospring51/Demospring51Application.java
+++ src/main/java/swat018/demospring51/Demospring51Application.java
@@ -2,22 +2,12 @@ package swat018.demospring51;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 @SpringBootApplication
+@EnableAsync
 public class Demospring51Application {
     public static void main(String[] args) {
         SpringApplication.run(Demospring51Application.class, args);
-    }
-
-    @Bean
-    public MessageSource messageSource() {
-        var messageSource = new ReloadableResourceBundleMessageSource();
-        messageSource.setBasename("classpath:/messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        messageSource.setCacheSeconds(3);
-        return messageSource;
     }
 }
