e0fc06a201f86d31750b1e95610775034e5fd3e0
jinwoo park <swat018@gmail.com>
2021-06-28 23:56:29 +0900

    Resource 추상화

--- src/main/java/swat018/demospring51/AppRunner.java
+++ src/main/java/swat018/demospring51/AppRunner.java
@@ -3,7 +3,10 @@ package swat018.demospring51;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
+import org.springframework.context.ApplicationContext;
 import org.springframework.context.ApplicationEventPublisher;
+import org.springframework.context.support.ClassPathXmlApplicationContext;
+import org.springframework.context.support.FileSystemXmlApplicationContext;
 import org.springframework.core.io.Resource;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Component;
@@ -15,11 +18,15 @@ import java.nio.file.Path;
 public class AppRunner implements ApplicationRunner {
 
     @Autowired
-    ResourceLoader resourceLoader;
+    ApplicationContext resourceLoader;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-       Resource resource = resourceLoader.getResource("classpath:test.txt");
+        System.out.println(resourceLoader.getClass());
+
+        Resource resource = resourceLoader.getResource("classpath:test.txt");
+        System.out.println(resource.getClass());
+
         System.out.println(resource.exists());
         System.out.println(resource.getDescription());
         System.out.println(Files.readString(Path.of(resource.getURI())));
