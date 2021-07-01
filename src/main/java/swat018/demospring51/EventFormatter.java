3d61478cd5497dfe586c949e563dce8f19479185
jinwoo park <swat018@gmail.com>
2021-07-01 14:15:07 +0900

    데이터 바인딩 추상화: Converter와 Formatter

---
+++ src/main/java/swat018/demospring51/EventFormatter.java
@@ -0,0 +1,23 @@
+package swat018.demospring51;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.format.Formatter;
+import org.springframework.stereotype.Component;
+
+import java.text.ParseException;
+import java.util.Locale;
+
+@Component
+public class EventFormatter implements Formatter<Event> {
+
+    @Override
+    public Event parse(String text, Locale locale) throws ParseException {
+        return new Event(Integer.parseInt(text));
+    }
+
+    @Override
+    public String print(Event object, Locale locale) {
+        return object.getId().toString();
+    }
+}
