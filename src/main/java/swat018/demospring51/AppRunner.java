c5edcd4147f2dbb5d477009473bbc2c6894cbf3a
jinwoo park <swat018@gmail.com>
2021-07-01 15:43:29 +0900

    SpEL (스프링 Expression Language)

--- src/main/java/swat018/demospring51/AppRunner.java
+++ src/main/java/swat018/demospring51/AppRunner.java
@@ -1,21 +1,55 @@
 package swat018.demospring51;
 
-import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.ApplicationArguments;
 import org.springframework.boot.ApplicationRunner;
-import org.springframework.core.convert.ConversionService;
+import org.springframework.expression.Expression;
+import org.springframework.expression.ExpressionParser;
+import org.springframework.expression.spel.standard.SpelExpressionParser;
 import org.springframework.stereotype.Component;
 
 @Component
 public class AppRunner implements ApplicationRunner {
 
-    @Autowired
-    ConversionService conversionService;
+    @Value("#{1 + 1}")
+    int value;
+
+    @Value("#{'hello ' + 'world'}")
+    String greeting;
+
+    @Value("#{1 eq 1}")
+    boolean trueOrFalse;
+
+    @Value("hello")
+    String hello;
+
+    @Value("${my.value}")
+    int myValue;
+
+    @Value("#{${my.value} eq 100}")
+    boolean isMyValue100;
+
+    @Value("#{'spring'}")
+    String spring;
+
+    @Value("#{sample.data}")
+    int sampleData;
 
     @Override
     public void run(ApplicationArguments args) throws Exception {
-        System.out.println(conversionService);
+        System.out.println("==============");
+        System.out.println(value);
+        System.out.println(greeting);
+        System.out.println(trueOrFalse);
+        System.out.println(hello);
+        System.out.println(myValue);
+        System.out.println(isMyValue100);
+        System.out.println(spring);
+        System.out.println(sampleData);
 
-        System.out.println(conversionService.getClass().toString());
+        ExpressionParser parser = new SpelExpressionParser();
+        Expression expression = parser.parseExpression("2 + 100");
+        Integer value = expression.getValue(Integer.class);
+        System.out.println(value);
     }
 }
---
+++ src/main/java/swat018/demospring51/Sample.java
@@ -0,0 +1,16 @@
+package swat018.demospring51;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class Sample {
+    private int data = 200;
+
+    public int getData() {
+        return data;
+    }
+
+    public void setData(int data) {
+        this.data = data;
+    }
+}
